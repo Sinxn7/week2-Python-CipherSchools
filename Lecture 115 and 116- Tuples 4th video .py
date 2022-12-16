439a7e61a2947c0247c3e1d484c0af398a9de805
Sinxn7 <119039133+Sinxn7@users.noreply.github.com>
2022-12-17 01:04:20 +0530

    Create Lecture 115 and 116- Tuples 4th video .py

---
+++ Lecture 115 and 116- Tuples 4th video .py
@@ -0,0 +1,9 @@
+nums = tuple(range(1,11))
+print(nums)
+#changing tuple to list
+nums = list((1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+print(type(nums))
+#changing tuple to string
+nums = str((1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+print(type(nums))
+print(nums)
