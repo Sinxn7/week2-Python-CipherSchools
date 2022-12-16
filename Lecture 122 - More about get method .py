bde64dc752585c1b42d0343b3a91989cee07ebc3
Sinxn7 <119039133+Sinxn7@users.noreply.github.com>
2022-12-17 00:56:51 +0530

    Create Lecture 122 - More about get method .py

---
+++ Lecture 122 - More about get method .py
@@ -0,0 +1,3 @@
+user_info={'name':'sinan','age':'18'}
+print(user_info.get('names',' not found ! '))
+# none ki jagah not found aayega agar names lika tou kyunki vo hamari dict. me keyword hai hi nhi
