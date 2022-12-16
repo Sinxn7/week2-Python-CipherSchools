b171387c58c50a99df1a93c6a5c94b45022617b2
Sinxn7 <119039133+Sinxn7@users.noreply.github.com>
2022-12-17 00:35:26 +0530

    Create Lecture 91- is vs equals in python .py

---
+++ Lecture 91- is vs equals in python .py
@@ -0,0 +1,5 @@
+fruits1=["Mango","Grapes","apple","chiku"]
+fruits3=["Mango","Grapes","apple","chiku","banana","kiwi","pear"]
+fruits2=["Mango","Grapes","apple","chiku"]
+print(fruits1==fruits2) #values are same
+print(fruits1 is fruits3) # false because it checks th location where the variable is stored if it is same then the result is true
