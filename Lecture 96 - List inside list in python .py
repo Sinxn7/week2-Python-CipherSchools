197f46326e7161a34fc2bb9e2ec728fb1f121ae7
Sinxn7 <119039133+Sinxn7@users.noreply.github.com>
2022-12-17 00:25:48 +0530

    Create Lecture 96 - List inside list in python .py

---
+++ Lecture 96 - List inside list in python .py
@@ -0,0 +1,16 @@
+matrix=[[1,2,3], [4,5,6], [7,8,9]] #2D list
+#3 items in list
+print(matrix[0])
+for i in matrix:
+    print(i)
+#print elemets of list separately
+for sublist in matrix:
+    #sublist me 1, 2 , 3 store hoga and further
+    for i in sublist:
+        print(i)
+
+print(matrix[1][1])
+
+s="string"
+print(type(s))
+print(type(matrix))
