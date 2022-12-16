a82b3880670f730e59b2cdfcd5dad68381d83026
Sinxn7 <119039133+Sinxn7@users.noreply.github.com>
2022-12-17 00:49:51 +0530

    Create Lecture 73 and 74 - Functions.py

---
+++ Lecture 73 and 74 - Functions.py
@@ -0,0 +1,14 @@
+def add_two(a,b):
+    return a+b
+
+total=add_two(1000,4)
+print(total)
+a=input("Enter a string:")
+b=input("Enter a string 2: ")
+print(add_two(a,b))
+
+#LEcture 74 - Return v/s print
+def add_three(a,b,c):
+    print(a+b+c)
+
+add_three(5,5,5)
