975c957b9174772cc77fb5c149642feb8b29b5be
Sinxn7 <119039133+Sinxn7@users.noreply.github.com>
2022-12-17 00:36:34 +0530

    Create Lecture 90 - Some more lists method in python .py

---
+++ Lecture 90 - Some more lists method in python .py
@@ -0,0 +1,12 @@
+fruits1=["Mango","Grapes","apple","chiku","banana","kiwi","pear"]
+print(fruits1.count("apple"))
+
+
+print(sorted(fruits1))
+
+
+numbers=[1,9,3,7,5,0]
+print(sorted(numbers))
+
+numbers_copy=numbers.copy
+print(numbers_copy)
