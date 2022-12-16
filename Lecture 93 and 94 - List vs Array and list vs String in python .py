844beb8a8111737ff8aa7708ce63db9d490179c4
Sinxn7 <119039133+Sinxn7@users.noreply.github.com>
2022-12-17 00:31:58 +0530

    Create Lecture 93 and 94 - List vs Array and list vs String in python .py

---
+++ Lecture 93 and 94 - List vs Array and list vs String in python .py
@@ -0,0 +1,14 @@
+#strings are immutable (cant change)
+    #cannot add characters
+#lists are mutable (can be changed)
+    #can add characters
+s="string"
+t=s.title()
+print(t)
+
+
+l=['word','word1','word2']
+l.pop(2)
+print(l)
+l.append('word3')
+print(l)
