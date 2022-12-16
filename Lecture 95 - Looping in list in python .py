3bc95fe926b3378c5fefcec14c0d6df987030f96
Sinxn7 <119039133+Sinxn7@users.noreply.github.com>
2022-12-17 00:31:10 +0530

    Create Lecture 95 - Looping in list in python .py

---
+++ Lecture 95 - Looping in list in python .py
@@ -0,0 +1,9 @@
+fruits1=["Mango","Grapes","apple","chiku","banana","kiwi","pear"]
+#for loop
+for fruit in fruits1:
+    print(fruit)
+#while loop
+i = 0 # kyunki list ki pehli item ki jo position hai vo zero hai
+while i<len(fruits1):
+    print(fruits1[i])
+    i+=1
