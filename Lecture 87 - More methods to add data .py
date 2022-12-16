164c42ef0b85b2a9c7800357c4a8f91cbd28e05d
Sinxn7 <119039133+Sinxn7@users.noreply.github.com>
2022-12-17 00:40:00 +0530

    Create Lecture 87 - More methods to add data .py

---
+++ Lecture 87 - More methods to add data .py
@@ -0,0 +1,8 @@
+fruits = ['oranges','apple']
+fruits.insert(1,"grapes")
+print(fruits)
+
+fruits1=["Mango","Grapes"]
+print(fruits.extend(fruits1))
+print(fruits.append(fruits1))
+print(fruits+fruits1)
