8e2df37bbff4b365d13a947fcbec57fd53dde5b0
Sinxn7 <119039133+Sinxn7@users.noreply.github.com>
2022-12-17 00:55:28 +0530

    Create Lecture 63 - For loop example 1.py

---
+++ Lecture 63 - For loop example 1.py
@@ -0,0 +1,11 @@
+# Lecture 63 - For loop example
+total=0
+for i in range(1,11):
+    total+=i
+print(total)
+
+n = int(input("enter the number: "))
+total = 0
+for i in range(1,n+1):
+    total+=i
+print(total)
